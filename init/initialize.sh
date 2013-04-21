e6c2a36a30e552b256374582fa82eb0c02c57405
stephen soltesz <stephen.soltesz@gmail.com>
2013-04-21 02:18:04 +0000

    update to remove epel pkg, remove 'sudo'

--- init/initialize.sh
+++ init/initialize.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# import epel release information & yum repo files
-sudo yum install -y http://mirror.pnl.gov/epel/6/i386/epel-release-6-8.noarch.rpm
+# NOTE: exit on error
+set -e
 # install libraries linked to gserver
-sudo yum install -y libpcap libcurl libmicrohttpd
+yum install -y libpcap libcurl libmicrohttpd
 
-sudo cp -f /home/mpisws_broadband/datapull_rename.py /etc/cron.hourly
-sudo cp -f /home/mpisws_broadband/cleanup.py /etc/cron.daily
+cp -f /home/mpisws_broadband/datapull_rename.py /etc/cron.hourly
